536f0e57572642c5b5ec1b7fe63a6acad99e2103
hiramtan <hiramtan@qq.com>
2014-07-22 16:39:11 +0800

    modify

---
+++ Platform_wp8/unity/Assets/Test.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using windowsphoneplugin;
+
+public class Test : MonoBehaviour
+{
+
+    string test;
+    // Use this for initialization
+    void Start()
+    {
+        test = windowsphoneplugin.Class1.GetDeviceName;
+        Debug.Log(test);
+    }
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(Screen.width*0.5f, Screen.height*0.5f, 300, 40), test);
+    }
+}
---
+++ Platform_wp8/visual/Admob/Admob/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using GoogleAds;
+using Microsoft.Phone.Controls;
+
+namespace Admob
+{
+    public class Class1 : PhoneApplicationPage
+    {
+          public MainPage()
+        {
+            InitializeComponent();
+            // NOTE: Edit "MY_AD_UNIT_ID" with your ad unit id.
+            AdView bannerAd = new AdView
+            {
+                Format = AdFormats.Banner,
+                AdUnitID = "MY_AD_UNIT_ID"
+            };
+            bannerAd.ReceivedAd += OnAdReceived;
+            bannerAd.FailedToReceiveAd += OnFailedToReceiveAd;
+            LayoutRoot.Children.Add(bannerAd);
+            AdRequest adRequest = new AdRequest();
+            adRequest.ForceTesting = true;
+            bannerAd.LoadAd(adRequest);
+        }
+    }
+}
